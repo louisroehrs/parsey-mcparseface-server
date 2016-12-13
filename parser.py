834a5d97fe551cfe0383fbcbb6122f8bb7d2830e
Louis Roehrs <lroehrs@us.ibm.com>
2016-12-13 05:04:27 -0800

    testing

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 import subprocess
 
 ROOT_DIR = "models/syntaxnet"
-PARSER_EVAL = "syntaxnet/parser_eval_forever"
+PARSER_EVAL = "syntaxnet/syntaxnet/parser_eval_forever"
 MODEL_DIR = "syntaxnet/models/parsey_mcparseface"
 
 def open_parser_eval(args):
